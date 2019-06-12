ed092160fd191b62438773875abbcbcffb569134
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-06-11 21:14:06 -0500

    nav con color

--- src/js/interactivity.js
+++ src/js/interactivity.js
@@ -1,6 +1,6 @@
   window.onscroll = () => {
     const nav = document.querySelector('.header');
-    if(this.scrollY < 700){
+    if(this.scrollY < 750){
         nav.classList.remove('active');
     } else{
         nav.classList.add('active');
